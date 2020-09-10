c435ae9da1b705a7c8048db4f6095cb5f31ec845
Samarth Goyal <samarth_niku@yahoo.co.in>
2020-09-10 15:19:06 +0530

    Added comments and prefabs

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -6,19 +6,24 @@ using UnityEngine.EventSystems;
 
 public class Ball : MonoBehaviour
 {
+    // Ball Physics Variables
     [SerializeField] float ballSpeed = 1000f;
     [SerializeField] float jumpSpeed = 1000f;
     [SerializeField] float touchInputForceDirection = 10f;
 
+    // Cached References
     private Touch touch;
     private Rigidbody ballRigidBody;
     private float screenWidth;
+
+    // State
     private bool ballTouched = false;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        // Initialize cached references
         ballRigidBody = GetComponent<Rigidbody>();
         screenWidth = Screen.width;
     }
@@ -31,29 +36,37 @@ public class Ball : MonoBehaviour
 
     private void GetTouchInput()
     {
+        // Get touch input
         if (Input.touchCount > 0)
         {
             touch = Input.GetTouch(0);
             switch (touch.phase)
             {
+                // When touch starts
                 case TouchPhase.Began:
+                    // Form raycast from camera to ball
                     Ray raycast = Camera.main.ScreenPointToRay(touch.position);
                     RaycastHit raycastHit;
                     if (Physics.Raycast(raycast, out raycastHit))
                     {
+                        // Make ball jump if raycast collides with it
                         if (raycastHit.collider.CompareTag("Ball"))
                         {
                             moveBallVertically(touchInputForceDirection);
+                            // change ball state 
                             ballTouched = true;
                         }
 
+                        // Move ball horizontally on the basis of touch position with respect to screen width
                         else
                         {
+                            // Move ball right
                             if (touch.position.x > screenWidth / 2)
                             {
                                 moveBallHorizontally(touchInputForceDirection);
                             }
 
+                            // Move ball left
                             else if (touch.position.x < screenWidth / 2)
                             {
                                 moveBallHorizontally(-touchInputForceDirection);
@@ -63,7 +76,9 @@ public class Ball : MonoBehaviour
 
                     break;
 
+                // When touch ends
                 case TouchPhase.Ended:
+                    // decrease ball speed on the basis of ball state
                     if (!ballTouched)
                     {
                         ballRigidBody.velocity = Vector3.zero;
@@ -74,17 +89,20 @@ public class Ball : MonoBehaviour
                         ballRigidBody.velocity *= 0.9f;
                         ballRigidBody.angularVelocity *= 0.9f;
                     }
+                    // revert back ball state
                     ballTouched = false;
                     break;
             }
         }
     }
 
+    // Make ball jump
     private void moveBallVertically(float touchInputForceDirection)
     {
         ballRigidBody.AddForce(new Vector3(0, touchInputForceDirection * jumpSpeed * Time.deltaTime, 0));
     }
 
+    // Make ball move horizontally
     private void moveBallHorizontally(float touchInputForceDirection)
     {
         ballRigidBody.AddForce(new Vector3(touchInputForceDirection * ballSpeed * Time.deltaTime, 0, 0));
